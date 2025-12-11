5411ebf18f8ec17a0d0150cbae04337bf171233f
Laya Myadam <164813734+Laya-Myadam@users.noreply.github.com>
2025-12-10 18:05:11 -0600

    Delete volunteer_matching directory

--- volunteer_matching/app/preprocessing.py
+++
@@ -1,8 +0,0 @@
-from sklearn.feature_extraction.text import TfidfVectorizer
-
-def prepare_tfidf(vol_texts, req_text):
-    vectorizer = TfidfVectorizer(stop_words="english")
-    docs = vol_texts + [req_text]
-
-    tfidf_matrix = vectorizer.fit_transform(docs)
-    return tfidf_matrix[:-1], tfidf_matrix[-1]
