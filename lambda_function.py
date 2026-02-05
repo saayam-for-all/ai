80df32db5c56faf0c431e5591f579ef623cfc6f7
Ujjwal Baranwal <pipkck@proton.me>
2026-02-04 20:35:52 -0700

    Modified how body is extracted from the event; renamed category_code with category_id; added a try-exception block on generate-ai_answer to identify any Errors; Added .csv in gitignore

--- lambda_function.py
+++ lambda_function.py
@@ -5,10 +5,20 @@ from utils.categories import help_categories
 
 def lambda_handler(event, context):
     try:
-        body = json.loads(event.get("body", "{}"))
+
+        # Identify the body from the event
+        
+        body = json.loads(raw_body) if isinstance(raw_body := event.get("body"), str) else event
+        
+        # Alt:
+        # raw_body = event.get("body")
+        # if isinstance(raw_body, str):
+        #    body = json.loads(raw_body) # Body is a JSON string - API Gateway
+        # else:
+        #    body = event # Body is a dictionary - Direct invocation
 
         # Extract fields from the request body
-        category_code = body.get("category")
+        category_id = body.get("category_id")
         subject = body.get("subject")
         description = body.get("description")
         location = body.get("location")
@@ -16,17 +26,18 @@ def lambda_handler(event, context):
         age = body.get("age")
         conversation_history = body.get("conversation_history")
 
-        # Required fields validation: Description, Subject, Category Code
-        required = {"description": description, "subject": subject, "category_code": category_code,}
+        # Required fields validation: Description, Subject, Category ID
+        required = {"description": description, "subject": subject, "category_id": category_id,}
         missing = [f"{k} missing" for k, v in required.items() if not v]
         if missing:
             return _response(400, {"error": ", ".join(missing)})
 
-        # Map category code to category string
-        category = help_categories.get(category_code) or "General"
+        # Map category id to category
+        category = help_categories.get(category_id) or "General"
 
         # Generate the AI answer using params
-        answer = generate_ai_answer(
+        try:
+            answer = generate_ai_answer(
             category=category,
             subject=subject,
             description=description,
@@ -34,12 +45,17 @@ def lambda_handler(event, context):
             gender=gender,
             age=age,
             conversation_history=conversation_history,
-        ) or "Error: Failed to generate answer"
+            )
+            if not answer:
+                raise ValueError("Error: Empty response")
+        except Exception:
+            answer = "Error: Failed to generate answer"
+
 
         # Return the generated answer
         return _response(
             200,
-            {"answer": answer,},
+            {"answer": answer,}, 
         )
 
     except Exception as e:
