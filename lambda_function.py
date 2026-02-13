1a0fba6bce5ec89dd09bcaf2e1b6fb5f9fa8cf0e
Rahulmandava4 <73526315+Rahulmandava4@users.noreply.github.com>
2026-02-13 15:15:21 -0800

    Removed json.dumps from Lambda response to avoid double encoding

--- lambda_function.py
+++ lambda_function.py
@@ -43,5 +43,5 @@ def _response(status_code, body):
             "Content-Type": "application/json",
             "Access-Control-Allow-Origin": "*"
         },
-        "body": json.dumps(body)
+        "body": body
     }
